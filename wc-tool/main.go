1c51afbe5d2d645d8b6026200b94d9a787e0f041
Akash Jain <ajcodes.tech@gmail.com>
2024-12-30 16:50:34 +0000

    second step

--- wc-tool/main.go
+++ wc-tool/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 const HELP_TEXT = `Usage wc [OPTION] [FILENAME]
 	-c, --bytes            print the byte counts
+	-l, --lines            print the newline counts
 `
 
 type CountCommand struct {
@@ -22,20 +24,46 @@ func (c CountCommand) execute() {
 	fmt.Printf("%d %s\n", len(bytes), c.input)
 }
 
+type LineCommand struct {
+	input string
+}
+
+func (c LineCommand) execute() {
+	bytes, err := os.ReadFile(c.input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	text := strings.Split(string(bytes), "\n")
+	fmt.Printf("%d %s\n", len(text), c.input)
+}
+
+func isCountCommand(command string) bool {
+	return (command == "-c" || command == "--bytes")
+}
+
+func isLineCommand(command string) bool {
+	return command == "-l" || command == "--lines"
+}
+
 func parseCommandLineArgs() {
 	args := os.Args[1:]
 	if len(args) != 2 {
 		fmt.Print(HELP_TEXT)
 		return
 	}
-	if args[0] != "-c" && args[0] != "--bytes" {
-		fmt.Print(HELP_TEXT)
-		return
+	if isCountCommand(args[0]) {
+		countCmd := &CountCommand{
+			input: args[1],
+		}
+		countCmd.execute()
 	}
-	countCmd := &CountCommand{
-		input: args[1],
+	if isLineCommand(args[0]) {
+		lineCmd := &LineCommand{
+			input: args[1],
+		}
+		lineCmd.execute()
 	}
-	countCmd.execute()
 }
 
 func main() {
